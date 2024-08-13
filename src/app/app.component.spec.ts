a76b16f4433fae08c11dc71599e6dfd1bbdd1c51
posthello-code <github@alexjensen.net>
2024-08-12 22:29:24 -0700

    fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,8 +24,8 @@ describe("AppComponent", () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector("h1")?.textContent).toContain(
-      "Hello, slacker-news",
+    expect(compiled.querySelector("#title-text")?.textContent).toContain(
+      "Slacker News",
     );
   });
 });
