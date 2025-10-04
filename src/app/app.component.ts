174ff7977561ff4468f780c7ee8b1362d527643f
posthello-code <github@alexjensen.net>
2025-10-04 11:34:15 -0700

    add dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { SplitterModule } from "primeng/splitter";
 import { TimelineModule } from "primeng/timeline";
 import { TimelineViewerComponent } from "./timeline-viewer/timeline-viewer.component";
 import { CommonModule, NgIf } from "@angular/common";
 import { debounce, fromEvent, interval } from "rxjs";
+import { ThemeService } from "./theme.service";
+import { ButtonModule } from "primeng/button";
+import { TooltipModule } from "primeng/tooltip";
 
 @Component({
   selector: "app-root",
@@ -16,18 +19,30 @@ import { debounce, fromEvent, interval } from "rxjs";
     TimelineViewerComponent,
     NgIf,
     CommonModule,
+    ButtonModule,
+    TooltipModule,
   ],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
 export class AppComponent {
+  @ViewChild(TimelineViewerComponent) timelineViewer!: TimelineViewerComponent;
   isSmallScreen = window.innerWidth < 768;
   title = "slacker-news";
-  constructor() {
+
+  constructor(public themeService: ThemeService) {
     const resize = fromEvent(window, "resize");
     const result = resize.pipe(debounce(() => interval(500)));
     result.subscribe(() => {
       this.isSmallScreen = window.innerWidth < 768;
     });
+  }
+
+  toggleDarkMode(): void {
+    this.themeService.toggleDarkMode();
+  }
+
+  get isDataLoading(): boolean {
+    return this.timelineViewer ? !this.timelineViewer.dataAvailable : true;
   }
 }
